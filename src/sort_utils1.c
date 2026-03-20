25889cc41e8ddfa91eb492acbad00dad9a4c432f
Dynsai <DynsaiCode@outlook.com>
2026-03-20 17:09:39 +0100

    Inicio del sorting

--- src/sort_utils1.c
+++ src/sort_utils1.c
@@ -1,13 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bitwise_sort.c                                     :+:      :+:    :+:   */
+/*   sort_utils1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: parenas- <parenas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 14:56:28 by parenas-          #+#    #+#             */
-/*   Updated: 2026/03/20 15:33:19 by parenas-         ###   ########.fr       */
+/*   Updated: 2026/03/20 17:06:53 by parenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/push_swap.h"
+
 //TODO sort de 3 y de 4
+void	sort_three(t_stack **a)
+{
+	if ((*a)->value > (*a)->next->value
+		&& (*a)->value > ft_lstlast(*a)->value)
+		sort_rotate
+		
+}
+
+void	sort_four(t_stack **a, t_stack **b)
+{
+
+}
